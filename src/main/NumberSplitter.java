0cec4caef4435687b3083f8607da23bc1826b3f8
Sergio <garciasergiomar@gmail.com>
2021-03-08 21:21:50 -0600

    initial commit

---
+++ src/main/NumberSplitter.java
@@ -0,0 +1,9 @@
+package main;
+
+public class NumberSplitter {
+    // escribir un metodo llamado split que acepte un numero entero y devuelva un array de números
+    // enteros, un numero por cada cifra
+    // ejemplo :
+    // entrada :  987654321
+    // salida : {9, 8, 7, 6, 5, 4, 3, 2, 1}
+}
---
+++ src/main/StringReverser.java
@@ -0,0 +1,5 @@
+package main;
+
+public class StringReverser {
+    // escribir un método llamado "reverse" que reciba una cadena de texto y regrese esa cadena en reversa
+}
